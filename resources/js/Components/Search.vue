683555598a476a48c57d26608c4917bd5d25bedf
4th-avenue <quebec706@gmail.com>
2024-02-06 15:25:33 +0900

    meilisearch에서 반환된 결과 표시하기

--- resources/js/Components/Search.vue
+++ resources/js/Components/Search.vue
@@ -28,7 +28,7 @@ onMounted(() => {
 
 const search = async (query) => {
     if(query) {
-        results.value = await client.value.index('posts').search(query);
+        results.value = await client.value.index('posts').search(query, {limit: 8});
         console.log(results.value);
     }
 };
@@ -55,6 +55,13 @@ watchEffect(() => {
                     <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
                         <DialogPanel class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                             <input type="search" v-model="query" class="w-full border-none block rounded-md" />
+                            <template v-if="results">
+                                Found
+                                <span v-text="results.estimatedTotalHits"></span> result(s)
+                                <a v-for="(hit, index) in results.hits" :key="index" :href="`/articles/${hit.id}`" class="block w-full py-2 px-3 border-b border-slate-200">
+                                    <h1>{{ hit.title }}</h1>
+                                </a>
+                            </template>
                         </DialogPanel>
                     </TransitionChild>
                 </div>
